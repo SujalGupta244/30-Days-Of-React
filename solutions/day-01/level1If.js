47de347db780d7cbabedff13be32e88c2f130ce0
Sujal gupta <sujalgupta5656@gmail.com>
2022-05-08 15:00:24 +0530

    chore: if else exercise level 1 complete

---
+++ solutions/day-01/level1If.js
@@ -0,0 +1,35 @@
+// IF ELSE LEVEL 1
+
+const myAge = 19;
+// let Ques = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// Ques.question(`Enter your age : `, ageNum => {
+// // console.log(`Hi ${name}!`);
+//     if(ageNum > myAge){
+//         console.log(`You are ${ageNum - myAge} years older than me.`);
+//     }else if(ageNum < myAge){
+//         console.log(`You are ${myAge - ageNum} years younger than me.`);
+//     }
+//     Ques.close();
+// });
+
+let a = 12, b = 3;
+if(a > b){
+    console.log("a is greater than b");
+}else{
+    console.log("a is less than b");
+}
+
+a>b? console.log("a is greater than b"): console.log("a is less than b");;
+// let age = prompt("Enter Your Age : ");
+
+
+
+
+
+
+
+
