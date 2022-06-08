ff31e08eb00b873f7ba44fbd66443b85dfe4f0ef
Sujal gupta <sujalgupta5656@gmail.com>
2022-06-08 18:30:20 +0530

    chore : exercise day 10 completed

---
+++ solutions/day-10/src/components/Navbar.js
@@ -0,0 +1,19 @@
+const Navbar = ()=>{
+    return (
+        <nav>
+            <h2 className="logo">Logo</h2>
+            <div className="navbar">
+                <ul className="navList">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Connect</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+
+export default Navbar;
---
+++ solutions/day-10/src/components/main.js
@@ -0,0 +1,19 @@
+const Main = () =>{
+    return (
+        <main>
+            <div className="hero">
+                <div className="hero-detail">
+                    <h2>I am a Developer</h2>
+                    <h3>Ready for work</h3>
+                    <div className="hero-btn">
+                        <button>Learn More</button>
+                        <button>Contact Me</button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+
+export default Main;
