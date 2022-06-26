010ceb7d1b2763c9af0518cdbaeff42f033c1ac5
Sujal gupta <sujalgupta5656@gmail.com>
2022-06-26 11:32:18 +0530

    chore : exercise day 23 completed

--- solutions/day-21/src/App.js
+++ solutions/day-21/src/App.js
@@ -1,4 +1,5 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
+import ReactDOM , {findDOMNode} from 'react-dom';
 import validator from 'validator';
 import './App.css';
 
@@ -32,6 +33,23 @@ const options = option.map((opt,index) =>{
   </option>
 })
 
+const Country = ({ country: { name, flag, population } }) => {
+  return (
+    <div className='country'>
+      <div className='country_flag'>
+        <img src={flag} alt={name} />
+      </div>
+      <h3 className='country_name'>{name.toUpperCase()}</h3>
+      <div class='country_text'>
+        <p>
+          <span>Population: </span>
+          {population}
+        </p>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   const initalState = {
     firstName : '',
@@ -44,6 +62,24 @@ function App() {
   }
 
   const [formData,setFormData] = useState(initalState);
+
+  const [data,setData] = useState([])
+
+  useEffect(()=>{
+    fetchData();
+  })
+
+  const fetchData = async () => {
+    const url = 'https://restcountries.eu/rest/v2/all'
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setData(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   let err = '';
   const handleChange = (e) =>{
     const {value,name} = e.target;
@@ -98,6 +134,10 @@ function App() {
         {/* {error.count && <span>{error.count}</span>}<br/> */}
         <button onClick={handleSubmit}>Submit</button>
       </form>
+      <br />
+      {data.map((country) => (
+            <Country country={country} />
+          ))}
   </div>)
 }
 
